6a7d533dc29c305dad0565bc4ead136296320a42
GN <GN@USER-PC>
2017-06-17 08:56:15 +0300

    First complete round of testing complete. No errors/bugs to report. Merge with Isothermal branch, soon.

--- MD simulation/Source.cpp
+++ MD simulation/Source.cpp
@@ -15,40 +15,32 @@ void StaticDataProcessing(size_t n);
 int main() {
   // freopen_s(&stream, "LOG.txt", "a+", stderr);
   size_t steps = 5000;
-  //vector<double> A_parameter = {0.0, 0.25, 0.5,  0.75, 1.0, 1.25, 1.50, 1.75,
-  //                              2.0, 2.25, 2.50, 2.75, 3.0, 3.5,  4.0};
-  vector<double> A_parameter = {/* 0.0, 0.25, 0.5, */ 0.75, 1.0, 1.25, 1.50, 2.0,  4.0 };
+  vector<double> A_par = {0.0, 0.25, 0.5,  0.75, 1.0, 1.25, 1.50, 1.75,
+                                2.0, 2.25, 2.50, 2.75, 3.0, 3.5,  4.0};
    //vector<size_t> power = {6, 7, 8, 9, 10, 12};
-  //vector<size_t> power = { 8, 10, 12 };
   vector<size_t> power = { 6};
   // cerr << "Log file of the MD simulation" << endl;
   // cerr << "Power:\tA:\tsteps:" << endl;
   size_t num = 1;
   char full = 'y';
-  // cout << "Run full simulation?"; cin >> full;
-  // Currently this code is not executing
- //  if (full == 'y')
-	// {
-	//      for (size_t n = 0; n < power.size(); n++)
-	//	   {
-	//		   for (size_t a = 0; a < A_parameter.size(); a++)
-	//		   {
-	//			   cout << "run: " << num << endl;
-	//			   //srand(time(NULL));
-	//			   std::auto_ptr<MD> run(new MD(power.at(n), A_parameter.at(a), steps));
-	//			   run->Simulation();
-	//			   ++num;
-	//			  /* MD run(power.at(n), A_parameter.at(a), steps);
-	//			   run.Simulation();
-	//			   ++num;*/
-	//			   //cerr << power.at(n) << "\t" << A_parameter.at(a) << "\t" <<  steps << "rho = 0.8" << endl;
-	//		   }
-	//		   //StaticDataProcessing(power.at(n));
-	//	   }
-	//}
   std::string dir = "../../Archives of Data/";
-  MD run(dir, 0.5, 5000);
-  run.Simulation(6, 0);
+   if (full == 'y')
+	 {
+	      for (size_t n = 0; n < power.size(); n++)
+		   {
+			   for (size_t a = 0; a < A_par.size(); a++)
+			   {
+				   cout << "run: " << num << endl;
+				   //srand(time(NULL));
+				   std::auto_ptr<MD> run(new MD(dir, 0.5, steps));
+				   run->Simulation(power.at(n), A_par.at(a));
+				   ++num;
+				   //cerr << power.at(n) << "\t" << A_parameter.at(a) << "\t" <<  steps << "rho = 0.8" << endl;
+			   }
+			   //StaticDataProcessing(power.at(n));
+		   }
+	}
+
   //std::auto_ptr<MD> run(new MD(6, 0, steps));
   //run->Simulation();
 

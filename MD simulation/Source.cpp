e074123623f8cceb9299666286b348d79c272b66
gn <giannis_nikiteas@hotmail.com>
2018-02-25 07:16:03 +0000

    linux thread error spotted

--- MD simulation/Source.cpp
+++ MD simulation/Source.cpp
@@ -5,7 +5,6 @@
 
 #define DENSITY 0.5
 #define STEPS 100
-#define DIR "./data/"
 
 //void MultiThread(MD class_object, std::vector<long double> n, std::vector<int> A){
 //  for (size_t i = 0; i < n.size(); i++){
@@ -18,7 +17,8 @@
 
 int main() {
   size_t num = 1;
-  std::string dir = DIR;
+  system ("mkdir ./data/");
+  std::string dir = "./data/";
   std::vector<int> p = { 6/* , 8, 10, 12  */};
   double a_value = 0.5 * std::pow(0.5, (2 / 12));
   // std::vector<double> A1 = { 0, 0.25, 0.50, 0.75, a_value, 1.00, 1.25, 1.50, 1.75, 2.00, 3, 4.00 };
@@ -26,22 +26,21 @@ int main() {
   std::vector<double> A2 = { 0.5, 0.75/*, 0.90, 0.95, 1.00, 1.05 */};
   std::vector<double> A3 = { 1., 1.25/*, 1.25, 1.50, 1.75, 2.00 */};
   std::vector<double> A4 = { 2, 2.5/*, 2.75, 3.0, 3.5, 4.0*/ };
-  MD run(DIR, 1.0, DENSITY, STEPS);
-  MD *run2 = new MD(DIR, 1.0, DENSITY, STEPS);
-  MD *run3 = new MD(DIR, 1.0, DENSITY, STEPS);
-  MD *run4 = new MD(DIR, 1.0, DENSITY, STEPS);
-  
-  system ("data/");
+  MD run(dir, 1.0, DENSITY, STEPS);
+  MD* run2 = new MD(dir, 1.0, DENSITY, STEPS);
+  MD* run3 = new MD(dir, 1.0, DENSITY, STEPS);
+  MD* run4 = new MD(dir, 1.0, DENSITY, STEPS);
+
   for (size_t i = 0; i < p.size(); i++) {
     for (size_t j = 0; j < A1.size(); j++) {
       //std::cout << "p: " << p[i] << " A: " << A1[j] << " run num: " << num << std::endl;
-      std::thread th(&MD::Simulation, run2, p[i], A1[j]);
-      std::thread th2(&MD::Simulation, run3, p[i], A3[j]);
-      std::thread th3(&MD::Simulation, run4, p[i], A4[j]);
+      // std::thread th(&MD::Simulation, run2, p[i], A2[j]);
+      // std::thread th2(&MD::Simulation, run3, p[i], A3[j]);
+      // std::thread th3(&MD::Simulation, run4, p[i], A4[j]);
       run.Simulation(p[i], A1[j]);
-      th.join();
-      th2.join();
-      th3.join();
+      // th.join();
+      // th2.join();
+      // th3.join();
       ++num;
     }
   }

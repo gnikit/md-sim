33a8209683769a0ef68ddb23f2b768098e456786
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-02-25 11:26:48 +0000

    small changes to qsub

--- src/md_job.sh
+++ src/md_job.sh
@@ -7,16 +7,19 @@
 # Load intel compiler
 module load intel-suite/2017.1 
 pwd
-# Loads initial velocities
+# Copy files to $TMPDIR
 dir="$HOME"/MD/MD-simulation/src
-cp "$dir"/{vx.txt, vy.txt, vz.txt, MD.h, MD.cpp, stat_analysis.h, stat_analysis.cpp, Source.cpp} $TMPDIR
+cd "$dir"
+cp vx.txt vy.txt vz.txt MD.h MD.cpp stat_analysis.h stat_analysis.cpp Source.cpp $TMPDIR
 
 cd $TMPDIR
 pwd 
 
+# Compile
 icpc -std=c++17 -O3 -use-intel-optimized-headers -qopenmp -pthread -parallel MD.cpp stat_analysis.cpp Source.cpp -o run_me.out
+# Run
 ./run_me.out
-OUTDIR="$WORK"/data
+OUTDIR="$HOME"/MD/data
 mkdir "$OUTDIR"
-# Copy files from temp to local file
+# Copy files from $TMPDIR to $WORK
 cp -r "$TMPDIR"/data/ "$OUTDIR"

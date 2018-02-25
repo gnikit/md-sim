b87744341c7d44be3b4c81ed1717ed4749b5ba81
gn <giannis_nikiteas@hotmail.com>
2018-02-25 11:12:42 +0000

    ready for cluster run

--- src/md_job.sh
+++ src/md_job.sh
@@ -9,9 +9,9 @@ module load intel-suite/2017.1
 pwd
 # Loads initial velocities
 dir="$HOME"/MD/MD-simulation/src
-cp "$dir"/{vx.txt, vy.txt, vz.txt, MD.h, MD.cpp, stat_analysis.h, stat_analysis.cpp, Source.cpp} $TEMP
+cp "$dir"/{vx.txt, vy.txt, vz.txt, MD.h, MD.cpp, stat_analysis.h, stat_analysis.cpp, Source.cpp} $TMPDIR
 
-cd $TEMP
+cd $TMPDIR
 pwd 
 
 icpc -std=c++17 -O3 -use-intel-optimized-headers -qopenmp -pthread -parallel MD.cpp stat_analysis.cpp Source.cpp -o run_me.out
@@ -19,4 +19,4 @@ icpc -std=c++17 -O3 -use-intel-optimized-headers -qopenmp -pthread -parallel MD.
 OUTDIR="$WORK"/data
 mkdir "$OUTDIR"
 # Copy files from temp to local file
-cp -r "$TEMP"/data/ "$OUTDIR"
+cp -r "$TMPDIR"/data/ "$OUTDIR"

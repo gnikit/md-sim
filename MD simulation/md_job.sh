c168063e53c8a5a00ff006d1c9af89075a4d66f7
gn <giannis_nikiteas@hotmail.com>
2018-02-25 06:29:36 +0000

    small changes to qsub script

--- MD simulation/md_job.sh
+++ MD simulation/md_job.sh
@@ -1,4 +1,5 @@
-#!usr/bin/bash
+#! /bin/bash
+
 #PBS -l walltime=00:00:30
 #PBS -l select=1:ncpus=4:mem=2gb
 
@@ -7,13 +8,15 @@
 module load intel-suite/2017.1 
 
 # Loads initial velocities
-cp $HOME/MD/MD-simulation/MD\ simulation/vx.txt $TEMP
-cp $HOME/MD/MD-simulation/MD\ simulation/vy.txt $TEMP
-cp $HOME/MD/MD-simulation/MD\ simulation/vy.txt $TEMP
+
+cp "$HOME"/MD/MD-simulation/MD\ simulation/vx.txt $TEMP
+cp "$HOME"/MD/MD-simulation/MD\ simulation/vy.txt $TEMP
+cp "$HOME"/MD/MD-simulation/MD\ simulation/vy.txt $TEMP
 
 
 icpc -std=c++17 -O3 MD.cpp stat_analysis.cpp Source.cpp -o run_me.out
 ./run_me.out
-
+OUTDIR="$WORK"/data
+mkdir "$OUTDIR"
 # Copy files from temp to local file
-cp -r $TEMP/data/ $WORK/data
+cp -r "$TEMP"/data/ "$OUTDIR"

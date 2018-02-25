a2c55c91d1cb7c30cf980d915f6e7284f07c58d8
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-02-25 03:17:53 +0000

    stat_analysis adjusted for new dir format

--- MD simulation/stat_analysis.cpp
+++ MD simulation/stat_analysis.cpp
@@ -1,10 +1,16 @@
 #include "stat_analysis.h"
 
-Stat_Analysis::Stat_Analysis(std::string PATH, vec1d A_LIST) {
+Stat_Analysis::Stat_Analysis(std::string PATH, vec1d A_LIST,
+                             size_t STEPS, size_t T, size_t PARTICLES,
+                             double DENSITY) {
   // Do everything in the constructor
   // No need for a Run function
-  _path = PATH;
+  _DIR = PATH;
   _A_list = A_LIST;
+  _STEPS = STEPS;
+  _T0 = T;
+  _N = PARTICLES;
+  _RHO = DENSITY;
 }
 
 Stat_Analysis::~Stat_Analysis() {}
@@ -24,8 +30,8 @@ void Stat_Analysis::ReadFromFile(vec1d &x, const std::string &file_name) {
   read_file.close();
 }
 
-void Stat_Analysis::ReadFromFile(vec1d &x, vec1d &y, vec1d &z,
-                                 vec1d &Xx, vec1d &Yy, vec1d &Zz, vec1d &w,
+void Stat_Analysis::ReadFromFile(vec1d &T, vec1d &K, vec1d &U,
+                                 vec1d &E, vec1d &Pc, vec1d &Pk, vec1d &P,
                                  const std::string &file_name) {
   /*
   Reads from a stream that already exists for a file that is already placed in the
@@ -57,11 +63,11 @@ void Stat_Analysis::ReadFromFile(vec1d &x, vec1d &y, vec1d &z,
         //ss >> g;
         //TODO: convert string STEP_NUM to int and resize vec1d to the appropriate size
         // add an index ++i that occupies vec1ds
-        x.push_back(a);
-        y.push_back(b);
-        z.push_back(c);
-        Xx.push_back(d);
-        Yy.push_back(e);
+        T.push_back(a);
+        K.push_back(b);
+        U.push_back(c);
+        E.push_back(d);
+        Pc.push_back(e);
         //Zz.push_back(f);
         //w.push_back(g);
       }
@@ -74,16 +80,16 @@ void Stat_Analysis::ReadFromFile(vec1d &x, vec1d &y, vec1d &z,
 
 }
 
-void Stat_Analysis::Mean(vec1d &x, vec1d &y, vec1d &z,
-                         vec1d &Xx, vec1d &Yy, vec1d &Zz, vec1d &w) {
+void Stat_Analysis::Mean(vec1d &T, vec1d &K, vec1d &U,
+                         vec1d &E, vec1d &Pc, vec1d &Pk, vec1d &P) {
   size_t n = 0;
 
-  for (size_t i = 0; i < x.size(); i++) {
-    _temp0 += x[i];
-    _temp1 += y[i];
-    _temp2 += z[i];
-    _temp3 += Xx[i];
-    _temp4 += Yy[i];
+  for (size_t i = 0; i < T.size(); i++) {
+    _temp0 += T[i];
+    _temp1 += K[i];
+    _temp2 += U[i];
+    _temp3 += E[i];
+    _temp4 += Pc[i];
     //_temp5 += Zz[i];
     //_temp6 += w[i];
     n++;
@@ -95,10 +101,9 @@ void Stat_Analysis::Mean(vec1d &x, vec1d &y, vec1d &z,
   _temp4 /= n;
   //_temp5 /= n;
   //_temp6 /= n;
-
 }
 
-void Stat_Analysis::StaticDataProcessing(size_t n)
+void Stat_Analysis::StaticDataProcessing(size_t POWER)
 /*
 Used to average the energies and pressures for all
 values of A for a specific number n of the potential
@@ -107,43 +112,58 @@ strength
 {
   vec1d temp;
 
+  std::stringstream A_stream;     // Fixing double to 2 decimals
+  std::stringstream rho_stream;
+  std::stringstream T_stream;
+  // TODO: setprecission function input here
+
+  T_stream << std::fixed << std::setprecision(1) << _T0;  // 1 decimal
+  rho_stream << std::fixed << std::setprecision(1) << _RHO;	// 1 decimal
+
+  // For consistency string streams should be placed here
+  _step_to_str = "_step_" + std::to_string(_STEPS); // constr
+  _particles_to_str = "_particles_" + std::to_string(_N); // constr
+  _rho_to_str = "_rho_" + rho_stream.str();   // constr
+  _T_to_str = "_T_" + T_stream.str();   // constr
+  _n_to_str = "_n_" + std::to_string(POWER);
+  _A_to_str = "_A_" + A_stream.str();
+
+  _FILE_ID = _step_to_str + _particles_to_str + _rho_to_str +
+    _T_to_str + _n_to_str;
+
   std::ofstream data;
-  std::string file_name, path, sep, power, a, txt;
-  path = _path;
+  std::string _FILE_NAME, sep, power, a, TXT;
   sep = "_";
-  txt = ".txt";
-  // path = "\0";
-  power = std::to_string(n);
-  std::string name = path + "AVGdata" + power + txt;
+  TXT = ".txt";
+  power = std::to_string(POWER);
+  std::string name = _DIR + "AVGdata" + _FILE_ID + TXT;
   data.open(name, std::ios::out | std::ios::trunc);
 
-  // data << "Power:\t" << n << std::endl;
   data << "# A:\tT\tK:\tU:\tETot:\tPc:" << std::endl;
 
   for (size_t i = 0; i < _A_list.size(); i++) {
-    file_name.clear();
-    TEMP0.clear();
-    TEMP1.clear();
-    TEMP2.clear();
-    TEMP3.clear();
-    TEMP4.clear();
-    TEMP5.clear();
-    TEMP6.clear();
-    file_name = "Data";
+    _FILE_NAME.clear();
+    T_vec.clear();
+    K_vec.clear();
+    U_vec.clear();
+    E_vec.clear();
+    Pc_vec.clear();
+    Pk_vec.clear();
+    P_vec.clear();
+    _FILE_NAME = "Data";
 
-    // Converts A to 2 decimal long double
+    // Converts A to 2 decimals
     std::stringstream stream;
-    stream << std::fixed << std::setprecision(2) << _A_list.at(i);
+    stream << std::fixed << std::setprecision(2) << _A_list[i];
 
-    a = stream.str();
-    file_name = path + file_name + power + sep + a + txt;
-    ReadFromFile(TEMP0, TEMP1, TEMP2, TEMP3, TEMP4, TEMP5, TEMP6, file_name);
-    Mean(TEMP0, TEMP1, TEMP2, TEMP3, TEMP4, TEMP5, TEMP6);
+    _A_to_str = "_A_" + stream.str();
+    _FILE_NAME = _DIR + _FILE_NAME + _FILE_ID + _A_to_str + TXT;
+    ReadFromFile(T_vec, K_vec, U_vec, E_vec, Pc_vec, Pk_vec, P_vec, _FILE_NAME);
+    Mean(T_vec, K_vec, U_vec, E_vec, Pc_vec, Pk_vec, P_vec);
     data.precision(5);
-    data << _A_list.at(i) << '\t' << _temp0 << '\t' << _temp1 << '\t' <<
+    // File writting
+    data << _A_list[i] << '\t' << _temp0 << '\t' << _temp1 << '\t' <<
       _temp2 << '\t' << _temp3 << '\t' << _temp4 << std::endl;
     // consider excluding usless vectors like PK, PTOT
-    // Debug to see if the method for file reading still works
-
   }
 }
--- MD simulation/stat_analysis.h
+++ MD simulation/stat_analysis.h
@@ -14,12 +14,18 @@ class Stat_Analysis {
 protected:
   typedef std::vector<double> vec1d;
   std::ifstream _data_reader;
+  size_t _N, _STEPS;  // Number of particles
+  double _RHO, _T0;
+
 
 private:
 
-  vec1d TEMP0, TEMP1, TEMP2, TEMP3, TEMP4, TEMP5, TEMP6;
+  vec1d T_vec, K_vec, U_vec, E_vec, Pc_vec, Pk_vec, P_vec;
   vec1d _A_list;
   std::string _path;
+  std::string _FILE_EXT;
+  std::string _step_to_str, _particles_to_str, _rho_to_str, _T_to_str, _n_to_str, _A_to_str;
+  std::string _DIR, _FILE_ID;
 
   long double _temp0 = 0;
   long double _temp1 = 0;
@@ -30,15 +36,15 @@ private:
   long double _temp6 = 0;
 
 public:
-  Stat_Analysis(std::string PATH, vec1d A_LIST);
+  Stat_Analysis(std::string PATH, vec1d A_LIST, size_t STEPS, size_t T, size_t PARTICLES, double DENSITY);
   ~Stat_Analysis();
 
   void ReadFromFile(vec1d &x, const std::string &file_name);
-  void ReadFromFile(vec1d &x, vec1d &y, vec1d &z,
-                    vec1d &Xx, vec1d &Yy, vec1d &Zz, vec1d &w,
+  void ReadFromFile(vec1d &T, vec1d &K, vec1d &U,
+                    vec1d &E, vec1d &Pc, vec1d &Pk, vec1d &P,
                     const std::string &file_name);
-  void Mean(vec1d &x, vec1d &y, vec1d &z,
-            vec1d &Xx, vec1d &Yy, vec1d &Zz, vec1d &w);
+  void Mean(vec1d &T, vec1d &K, vec1d &U,
+            vec1d &E, vec1d &Pc, vec1d &Pk, vec1d &P);
   void StaticDataProcessing(size_t n);
   // TODO: make a RunMe method and add arguments to the existing methods for increased reusability
 

b79b0f651f660a29ec9623b81eb6457de157780f
gn <giannis_nikiteas@hotmail.com>
2018-02-25 06:06:25 +0000

    complete qsub md_job.sh

--- MD simulation/md_job.sh
+++ MD simulation/md_job.sh
@@ -3,11 +3,17 @@
 #PBS -l select=1:ncpus=4:mem=2gb
 
 
-#Load intel compiler
+# Load intel compiler
 module load intel-suite/2017.1 
 
+# Loads initial velocities
 cp $HOME/MD/MD-simulation/MD\ simulation/vx.txt $TEMP
 cp $HOME/MD/MD-simulation/MD\ simulation/vy.txt $TEMP
 cp $HOME/MD/MD-simulation/MD\ simulation/vy.txt $TEMP
 
 
+icpc -std=c++17 -O3 MD.cpp stat_analysis.cpp Source.cpp -o run_me.out
+./run_me.out
+
+# Copy files from temp to local file
+cp -r $TEMP/data/ $WORK/data

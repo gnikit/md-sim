7838d9d71fc4611d8dcbe55a934b9bd4dc694c6b
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-02-27 01:12:26 +0000

    ready for 8 core cluster

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,10 +1,10 @@
 #include "MD.h"
 // #include "stat_analysis.h"
-// #include <thread>
+#include <thread>
 #include <string>
 
 #define DENSITY 0.5
-#define STEPS 10000
+#define STEPS 100
 
 //void MultiThread(MD class_object, std::vector<long double> n, std::vector<int> A){
 //  for (size_t i = 0; i < n.size(); i++){
@@ -14,17 +14,37 @@
 //    }
 //  }
 //}
+std::vector<double> LinearSpacedArray(double a, double b, std::size_t N)
+    {
+        double h = (b - a) / static_cast<double>(N-1);
+        std::vector<double> xs(N);
+        std::vector<double>::iterator x;
+        double val;
+        for (x = xs.begin(), val = a; x != xs.end(); ++x, val += h) {
+            *x = val;
+        }
+        return xs;
+    }
 
 int main() {
   size_t num = 1;
-  std::string dir = "../../Archives of Data/";  // Current Working Directory
+  std::string dir = "";  // Current Working Directory
   std::vector<int> p = { 6/* , 8, 10, 12 */ };
   double a_value = 0.5 * std::pow(0.5, (2 / 12));
   // std::vector<double> A1 = { 0, 0.25, 0.50, 0.75, a_value, 1.00, 1.25, 1.50, 1.75, 2.00, 3, 4.00 };
-  std::vector<double> A1 = { 0.0/* , 0.25, 0.50, 0.65, 0.70, 0.75 */};
-  std::vector<double> A2 = { 0.5/* , 0.75, 0.90, 0.95, 1.00, 1.05 */};
-  std::vector<double> A3 = { 1.0/* , 1.25, 1.25, 1.50, 1.75, 2.00 */};
-  std::vector<double> A4 = { 2.0/* , 2.50, 2.75, 3.00, 3.50, 4.00 */};
+  // std::vector<double> A1 = { 0.0/* , 0.25, 0.50, 0.65, 0.70, 0.75 */};
+  // std::vector<double> A2 = { 0.5/* , 0.75, 0.90, 0.95, 1.00, 1.05 */};
+  // std::vector<double> A3 = { 1.0/* , 1.25, 1.25, 1.50, 1.75, 2.00 */};
+  // std::vector<double> A4 = { 2.0/* , 2.50, 2.75, 3.00, 3.50, 4.00 */};
+  std::vector<double> A1 = LinearSpacedArray(0,1,5);
+  std::vector<double> A2 = LinearSpacedArray(1,2,5);
+  std::vector<double> A3 = LinearSpacedArray(2,4,5);
+  std::vector<double> A4 = LinearSpacedArray(5,10,5);
+  std::vector<double> A5 = LinearSpacedArray(0,1,5);
+  std::vector<double> A6 = LinearSpacedArray(1,2,5);
+  std::vector<double> A7 = LinearSpacedArray(2,4,5);
+  std::vector<double> A8 = LinearSpacedArray(5,10,5);
+
   MD run(dir, 1.0, DENSITY, STEPS);
   MD* run2 = new MD(dir, 1.0, 0.5, STEPS);
   MD* run3 = new MD(dir, 0.5, 0.5, STEPS);
@@ -37,17 +57,26 @@ int main() {
   for (size_t i = 0; i < p.size(); i++) {
     for (size_t j = 0; j < A1.size(); j++) {
       std::cout << "p: " << p[i] << " A: " << A1[j] << " run num: " << num << std::endl;
-      // std::thread th(&MD::Simulation, run2, p[i], A2[j]);
-      // std::thread th2(&MD::Simulation, run3, p[i], A3[j]);
-      // std::thread th3(&MD::Simulation, run4, p[i], A4[j]);
+      std::thread th2(&MD::Simulation, run2, p[i], A2[j]);
+      std::thread th3(&MD::Simulation, run3, p[i], A3[j]);
+      std::thread th4(&MD::Simulation, run4, p[i], A4[j]);
+      std::thread th5(&MD::Simulation, run5, p[i], A5[j]);
+      std::thread th6(&MD::Simulation, run6, p[i], A6[j]);
+      std::thread th7(&MD::Simulation, run7, p[i], A7[j]);
+      std::thread th8(&MD::Simulation, run8, p[i], A8[j]);
       run.Simulation(p[i], A1[j]);
-      // th.join();
-      // th2.join();
-      // th3.join();
+
+      th2.join();
+      th3.join();
+      th4.join();
+      th5.join();
+      th6.join();
+      th7.join();
+      th8.join();
       ++num;
     }
   }
-  delete run2, run3, run4;
+  delete run2, run3, run4, run5, run6, run7, run8;
   //std::vector<double> a;
   //a.reserve(A1.size() + A2.size() + A3.size() + A4.size());
   //a.reserve(A1.size() + A2.size());
--- src/local_run.sh
+++ src/local_run.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
 
-icpc -std=c++17 -Ofast -use-intel-optimized-headers -m64 -mkl=sequential -fp-model=precise -ipo -shared-intel -ipp-link=static -qopt-subscript-in-range -qopenmp -fstack-protector -mtune=core-avx-i MD.h MD.cpp Source.cpp -o a.out
-# -mgpu-asm-dump \
-# -daal \
-# -tbb \
+icpc -std=c++17 -O3 -Ofast -use-intel-optimized-headers -m64 -daal -tbb -mkl=parallel -fp-model=precise -ipo -shared-intel -ipp-link=static -qopt-subscript-in-range -qopenmp -fstack-protector -mtune=core-avx-i -mgpu-asm-dump MD.h MD.cpp Source.cpp -o a.out
+

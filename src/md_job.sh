7f65bafa4a2fd2c3b407fe04de318bd96141423a
gn <giannis_nikiteas@hotmail.com>
2018-02-27 01:46:41 +0000

    md_job.sh file fixed

--- src/md_job.sh
+++ src/md_job.sh
@@ -19,8 +19,8 @@ OUTDIR="$HOME"/MD/data
 mkdir "$OUTDIR"
 
 # Compile
-icpc -std=c++17 -O3 -use-intel-optimized-headers -qopenmp -pthread -parallel MD.cpp stat_analysis.cpp Source.cpp -o run_me.out
+icpc -std=c++17 -O3 -Ofast -xHost -complex-limited-range -use-intel-optimized-headers -parallel -m64 -daal -tbb -mkl=parallel -fp-model=precise -ipo -shared-intel -ipp-link=static -qopt-subscript-in-range -qopenmp -fstack-protector -vec-guard-write -vecabi=cmdtarget -ax=SSE3 -mtune=core-avx2 -qopt-multi-version-aggressive -qopt-matmul -qopenmp-lib=compat -qopenmp-link=static -qopenmp-threadprivate=legacy -prec-sqrt -fast-transcendentals MD.h MD.cpp Source.cpp -o a.out
 # Run
-./run_me.out
+./a.out
 # Copy files from $TMPDIR to $WORK
 cp * "$WORK"

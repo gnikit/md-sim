6f82d88d571c51ca1b325506d010be10f5ff5b11
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-02-28 16:17:34 +0000

    icc cluster test ready

--- src/md_job.sh
+++ src/md_job.sh
@@ -20,7 +20,7 @@ OUTDIR="$HOME"/MD/data
 mkdir "$OUTDIR"
 
 # Compile
-icpc -std=c++17 -O3 -Ofast -xHost -complex-limited-range -use-intel-optimized-headers -parallel -m64 -daal -tbb -mkl=parallel -fp-model=precise -ipo -shared-intel -ipp-link=static -qopt-subscript-in-range -qopenmp -fstack-protector -vec-guard-write -vecabi=cmdtarget -ax=SSE3 -mtune=core-avx2 -qopt-multi-version-aggressive -qopt-matmul -qopenmp-lib=compat -qopenmp-link=static -qopenmp-threadprivate=legacy -prec-sqrt -fast-transcendentals MD.h MD.cpp Source.cpp -o a.out
+icc -std=c++17 -parallel -O3 -use-intel-optimized-headers MD.h MD.cpp Source.cpp -o a.out
 # Run
 ./a.out
 # Copy files from $TMPDIR to $WORK

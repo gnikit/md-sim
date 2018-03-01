a844111e6b4226a7e68f4325d7f3e1e00c5c2a89
gn <giannis_nikiteas@hotmail.com>
2018-03-01 15:15:44 +0000

    small changes

--- src/md_job.sh
+++ src/md_job.sh
@@ -16,12 +16,12 @@ cp vx.txt vy.txt vz.txt MD.h MD.cpp stat_analysis.h stat_analysis.cpp Source.cpp
 cd $TMPDIR
 pwd
 # Dir created in case of early termination
-OUTDIR="$HOME"/MD/data
+OUTDIR="$HOME"/MD/Data
 mkdir "$OUTDIR"
 
 # Compile
 # icpc -std=c++17 -parallel -O3 -use-intel-optimized-headers MD.h MD.cpp Source.cpp -o a.out
-icpc -fast -std=c++11 -parallel -use-intel-optimized-headers MD.cpp Source.cpp -o a.out
+icpc -fast -std=c++17 -parallel -use-intel-optimized-headers MD.cpp Source.cpp -o a.out
 # Run
 ./a.out
 # Copy files from $TMPDIR to $WORK

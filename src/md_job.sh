b2bb35b5a460cca2f2325d49bf91baa831caee6d
GiannisNikiteas <giannis_nikiteas@hotmail.com>
2018-03-07 14:08:23 +0000

    small changes

--- src/md_job.sh
+++ src/md_job.sh
@@ -15,14 +15,16 @@ cp vx.txt vy.txt vz.txt MD.h MD.cpp stat_analysis.h stat_analysis.cpp Source.cpp
 # IMPORTANT: SHELL DIR HAS TO BE SAME AS VX, VY, VZ
 cd $TMPDIR
 pwd
-# Dir created in case of early termination
-OUTDIR="$HOME"/MD/Data
-mkdir "$OUTDIR"
+
 LIB=/apps/intel/2017.1/compilers_and_libraries_2017.1.132/linux/compiler/include
 # Compile
 icpc -pthread -parallel -mkl=parallel -O3 -xHOST -std=c++17 -use-intel-optimized-headers -m64 -prec-sqrt -prec-div MD.cpp Source.cpp -o a.out
 # Run
 ./a.out
-# Copy files from $TMPDIR to $WORK
 
+# Dir created in case of early termination
+OUTDIR="$HOME"/MD/Data
+mkdir "$OUTDIR"
+
+# Copy files from $TMPDIR to $WORK
 cp * "$OUTDIR"

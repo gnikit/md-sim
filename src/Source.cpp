32e1a7981fe2f05dce47f046876e496bfdeab047
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-03-20 15:15:24 +0000

    test-ready for multiple isomorph planes

--- src/Source.cpp
+++ src/Source.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <tuple>
 
-#define STEPS 10000
+#define STEPS 50000
 typedef std::vector<double> vec1d;
 
 // TODO: Move to another file?
@@ -78,57 +78,95 @@ int main() {
   /* Working directory of the cx1 cluster */
   std::string dir = "";
   /* Potential power strength */
-  //size_t n = 8;
-  ///* Generate Temperature vector for isomorph */
-  //vec1d T_iso = LinearSpacedArray(0.6, 3, 5);
-  ///* Empty containers for density and a_par */
-  //vec1d rho_iso, A_iso;
-  ///* Generate Density and A isomorph vectors */
-  //Isomorph isomorph_line(0.5, 0.5, 0.5, T_iso);
-  //std::tie(rho_iso, A_iso) = isomorph_line.GenLine(n);
+  size_t n = 8;
+  /* Generate Temperature vector for isomorph */
+  vec1d T_iso = LinearSpacedArray(0.5, 3, 5);
+  /* Empty containers for density and a_par */
+  vec1d rho_iso, A_iso;
+  vec1d rho_iso_h, A_iso_h;
+  vec1d rho_iso_k, A_iso_k;
+  vec1d rho_iso_l, A_iso_l;
+  /* Generate Density and A isomorph vectors */
+  Isomorph isomorph_line(0.5, 0.5, 1, T_iso);
+  std::tie(rho_iso, A_iso) = isomorph_line.GenLine(n);
+
+  Isomorph isomorph_line_h(0.5, 0.5, 0.75, T_iso);
+  std::tie(rho_iso_h, A_iso_h) = isomorph_line_h.GenLine(n);
+
+  Isomorph isomorph_line_k(0.5, 0.5, 1.25, T_iso);
+  std::tie(rho_iso_k, A_iso_k) = isomorph_line_k.GenLine(n);
+
+  Isomorph isomorph_linr_l(0.5, 0.5, 2.00, T_iso);
+  std::tie(rho_iso_l, A_iso_l) = isomorph_linr_l.GenLine(n);
   /*
   * This is an isomorph line run
   * Simulates the fluid along the line
   */
-  //for (size_t i = 0; i < T_iso.size(); i++) {
-  //  std::cout << "T: " << T_iso[i] << " rho: " << rho_iso[i] << " A: " << A_iso[i] << std::endl;
-  //  MD run(dir_windows, STEPS);
-  //  run.Simulation(rho_iso[i], T_iso[i], n, A_iso[i]);
-  //}
-  /*-----------------------------------------------*/
-  std::vector<size_t> n = { 6/*, 8, 10, 12*/ };
-  std::vector<double> rho = { 0.5, 1.0, 1.5, 2.0 }; 
-  std::vector<double> T = { 0.5, 1.0, 1.5, 2.0  };  
-  std::vector<double> A1 = LinearSpacedArray(0, 1, 5);
-  std::vector<double> A2 = LinearSpacedArray(1.25, 2.25, 5);
-  std::vector<double> A3 = LinearSpacedArray(2.50, 4.50, 5);  
-  std::vector<double> A4 = LinearSpacedArray(0.6, 1.2, 5);       
-  
-  
-  for (size_t d = 0; d < rho.size(); d++) {
-    for (size_t t = 0; t < T.size(); t++) {
-      for (size_t i = 0; i < n.size(); i++) {
-        for (size_t j = 0; j < A1.size(); j++) {
-          std::cout << "p: " << n[i] << " A: " << A1[j] << " run num: " << num << std::endl;
-  
-          MD* run1 = new MD(dir, STEPS);
-          MD* run2 = new MD(dir, STEPS);
-          MD* run3 = new MD(dir, STEPS);
-          MD* run4 = new MD(dir, STEPS);
-  
-          std::thread th1(&MD::Simulation, run1, rho[d], T[t], n[i], A3[j]);
-          std::thread th2(&MD::Simulation, run2, rho[d], T[t], n[i], A2[j]);
-          std::thread th3(&MD::Simulation, run3, rho[d /*+ (rho.size() / 2)*/], T[t], n[i], A3[j]);
-          std::thread th4(&MD::Simulation, run4, rho[d /*+ (rho.size() / 2)*/], T[t], n[i], A4[j]);
-  
-          th1.join(); th2.join(); th3.join(); th4.join();
-          delete run1, run2, run3, run4;
-  
-          ++num;
-        }
-      }
-    }
+  for (size_t i = 0; i < T_iso.size(); i++) {
+    std::cout << "T: " << T_iso[i] << " rho: " << rho_iso[i] << " A: " << A_iso[i] << std::endl;
+    MD* run1 = new MD(dir, STEPS);
+    MD* run2 = new MD(dir, STEPS);
+    MD* run3 = new MD(dir, STEPS);
+    MD* run4 = new MD(dir, STEPS);
+
+    std::thread th1(&MD::Simulation, run1, rho_iso[i], T_iso[i], n, A_iso[i]);
+    std::thread th2(&MD::Simulation, run2, rho_iso_h[i], T_iso[i], n, A_iso_h[i]);
+    std::thread th3(&MD::Simulation, run3, rho_iso_k[i], T_iso[i], n, A_iso_k[i]);
+    std::thread th4(&MD::Simulation, run4, rho_iso_l[i], T_iso[i], n, A_iso_l[i]);
   }
+  /*-----------------------------------------------*/
+  //std::vector<size_t> n = { 6, 8, 10, 12 };
+  //std::vector<double> rho = { 0.5, 1.0, 1.5, 2.0 }; 
+  //std::vector<double> T = { 0.5, 1.0, 1.5, 2.0  };  
+  //std::vector<double> A1 = LinearSpacedArray(0, 1, 5);
+  //std::vector<double> A2 = LinearSpacedArray(1.25, 2.25, 5);
+  //std::vector<double> A3 = LinearSpacedArray(2.50, 4.50, 5);  
+  //std::vector<double> A4 = LinearSpacedArray(0.6, 1.2, 5);       
+  //
+  //for (size_t d = 0; d < rho.size(); d++) {
+  //  for (size_t t = 0; t < T.size(); t++) {
+  //    std::cout << " run num: " << num << std::endl;
+  //    
+  //    MD* run1 = new MD(dir_windows, STEPS);
+  //    MD* run2 = new MD(dir_windows, STEPS);
+  //    MD* run3 = new MD(dir_windows, STEPS);
+  //    MD* run4 = new MD(dir_windows, STEPS);
+  //    
+  //    std::thread th1(&MD::Simulation, run1, rho[d], T[t], 6, 1);
+  //    std::thread th2(&MD::Simulation, run2, rho[d], T[t], 8, 1);
+  //    std::thread th3(&MD::Simulation, run3, rho[d], T[t], 10, 1);
+  //    std::thread th4(&MD::Simulation, run4, rho[d], T[t], 12, 1);
+  //    
+  //    th1.join(); th2.join(); th3.join(); th4.join();
+  //    delete run1, run2, run3, run4;
+  //    
+  //    ++num;
+  //  }
+  //}
+  //for (size_t d = 0; d < rho.size(); d++) {
+  //  for (size_t t = 0; t < T.size(); t++) {
+  //    for (size_t i = 0; i < n.size(); i++) {
+  //      for (size_t j = 0; j < A1.size(); j++) {
+  //        std::cout << "p: " << n[i] << " A: " << A1[j] << " run num: " << num << std::endl;
+  //
+  //        MD* run1 = new MD(dir, STEPS);
+  //        MD* run2 = new MD(dir, STEPS);
+  //        MD* run3 = new MD(dir, STEPS);
+  //        MD* run4 = new MD(dir, STEPS);
+  //
+  //        std::thread th1(&MD::Simulation, run1, rho[d], T[t], n[i], A1[j]);
+  //        std::thread th2(&MD::Simulation, run2, rho[d], T[t], n[i], A2[j]);
+  //        std::thread th3(&MD::Simulation, run3, rho[d], T[t], n[i], A3[j]);
+  //        std::thread th4(&MD::Simulation, run4, rho[d], T[t], n[i], A4[j]);
+  //
+  //        th1.join(); th2.join(); th3.join(); th4.join();
+  //        delete run1, run2, run3, run4;
+  //
+  //        ++num;
+  //      }
+  //    }
+  //  }
+  //}
   //std::vector<double> a;
   //a.reserve(A1.size() + A2.size() /*+ A3.size() + A4.size()*/);
   //a.reserve(A1.size() + A2.size());

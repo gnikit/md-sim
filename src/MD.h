d2ebc18025e7c3c3e532b5339d3e1974536c7595
gn <giannis_nikiteas@hotmail.com>
2018-06-05 11:40:35 +0100

    new binaries, changes to makefile

--- src/MD.h
+++ src/MD.h
@@ -1,19 +1,19 @@
 //////////////////////////////////////////////////////////////////////
-// Ioannis Nikiteas 13/7/2017									    //
-//																    //
-// BSc Dissertation:										        //
-//  Investigating the transition from Molecular Dynamics to		    //
-//	Smoothed Particle Hydrodynamics					    		    //
-//																    //
-//	University: Royal Holloway University of London		            //
-//																    // 
-//	A program meant to simulate a MD fluid with an only	            //
-//	repulsive pair-potential. Increasing the parameter A		 	//
-//	creates a coarse-graining effect for the system allowing it		//
-//	to transition to SPH											//
-//																	//
-//																	//
-//																	//
+// Ioannis Nikiteas 13/7/2017                                       //
+//                                                                  //
+// BSc Dissertation:                                                //
+//  Investigating the transition from Molecular Dynamics to         //
+//	Smoothed Particle Hydrodynamics                                 //
+//                                                                  //
+//	University: Royal Holloway University of London                 //
+//                                                                  // 
+//	A program meant to simulate a MD fluid with an only             //
+//	repulsive pair-potential. Increasing the parameter A            //
+//	creates a coarse-graining effect for the system allowing it     //
+//	to transition to SPH                                            //
+//                                                                  //
+//                                                                  //
+//                                                                  //
 //////////////////////////////////////////////////////////////////////
 #pragma once
 #if defined (__INTEL_COMPILER)

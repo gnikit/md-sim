5588fd4d9f173926006530d86cc800bac631944f
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-07-24 02:53:18 +0100

    Static Library is now complete.

--- examples/make_database.cpp
+++ examples/make_database.cpp
@@ -1,10 +1,10 @@
 #include <string>
 #include <thread>
-#include "../MD.h"
-#include "../stat_analysis.h"
+#include "MD.h"
+#include "stat_analysis.h"
 
-#define STEPS 10000     //10000
-#define PARTICLES 1000  //1000
+#define STEPS 10000     // 10000
+#define PARTICLES 1000  // 1000
 typedef std::vector<double> vec1d;
 
 std::string dir_linux = "./example_data/";
@@ -13,17 +13,15 @@ void MakeDataBase();
 
 std::vector<double> LinearSpacedArray(double a, double b, std::size_t N);
 
-int main() {
-  MakeDataBase();
-}
+int main() { MakeDataBase(); }
 
 void MakeDataBase() {
   /*
-	* Builds a relatively complete database mapping the rho,T,n,a space
-	* of te MD fluid. Performs Also statistical averaging at the end with
-	* quantities such as Pc, U, K, E, Pk.
-	* NOTE: It takes a long time to complete and generates multiple files!
-	*/
+   * Builds a relatively complete database mapping the rho,T,n,a space
+   * of te MD fluid. Performs Also statistical averaging at the end with
+   * quantities such as Pc, U, K, E, Pk.
+   * NOTE: It takes a long time to complete and generates multiple files!
+   */
   size_t num = 1;
   std::vector<size_t> n = {6, 8, 10, 12};
   std::vector<double> rho = {0.5, 1.0, 1.5, 2.0};
@@ -31,13 +29,15 @@ void MakeDataBase() {
   std::vector<double> A1 = LinearSpacedArray(0, 1, 5);
   std::vector<double> A2 = LinearSpacedArray(1.25, 2.25, 5);
   std::vector<double> A3 = LinearSpacedArray(2.50, 4.50, 5);
-  std::vector<double> A4 = LinearSpacedArray(0.6, 1.0, 5);  // Includes a=1.0 again
+  std::vector<double> A4 =
+      LinearSpacedArray(0.6, 1.0, 5);  // Includes a=1.0 again
 
   for (size_t d = 0; d < rho.size(); d++) {
     for (size_t t = 0; t < T.size(); t++) {
       for (size_t i = 0; i < n.size(); i++) {
         for (size_t j = 0; j < A1.size(); j++) {
-          std::cout << "rho: " << rho[d] << " T: " << T[t] << "n: " << n[i] << " A: " << A1[j] << " run num: " << num << std::endl;
+          std::cout << "rho: " << rho[d] << " T: " << T[t] << "n: " << n[i]
+                    << " A: " << A1[j] << " run num: " << num << std::endl;
 
           MD* run1 = new MD(dir_linux, STEPS);
           MD* run2 = new MD(dir_linux, STEPS);
@@ -47,7 +47,7 @@ void MakeDataBase() {
           std::thread th1(&MD::Simulation, run1, rho[d], T[t], n[i], A1[j]);
           std::thread th2(&MD::Simulation, run2, rho[d], T[t], n[i], A2[j]);
           std::thread th3(&MD::Simulation, run3, rho[d], T[t], n[i], A3[j]);
-          //std::thread th4(&MD::Simulation, run4, rho[d], T[t], n[i], A4[j]);
+          // std::thread th4(&MD::Simulation, run4, rho[d], T[t], n[i], A4[j]);
 
           th1.join();
           th2.join();
@@ -73,9 +73,10 @@ void MakeDataBase() {
   // // This bit of code has not been tested in its current 2-loop 1-loop format
   // for (size_t d = 0; d < rho.size(); d++) {
   //   for (size_t t = 0; t < T.size(); t++) {
-  //     // Defining in Heap to avoid obj corruption due to multiple optimisation
-  //     Stat_Analysis* data_averaging = new Stat_Analysis(dir_linux, STEPS, PARTICLES, 2.0, 1.5, a);
-  //     for (size_t i = 0; i < n.size(); i++) {
+  //     // Defining in Heap to avoid obj corruption due to multiple
+  //     optimisation Stat_Analysis* data_averaging = new
+  //     Stat_Analysis(dir_linux, STEPS, PARTICLES, 2.0, 1.5, a); for (size_t i
+  //     = 0; i < n.size(); i++) {
   //       data_averaging->StaticDataProcessing(n[i]);
   //     }
   //     delete data_averaging;
@@ -85,9 +86,9 @@ void MakeDataBase() {
 
 std::vector<double> LinearSpacedArray(double a, double b, std::size_t N) {
   /*
-	* Produces an equally spaced vector of N increments
-	* in the inclusive range of [a, b]
-	*/
+   * Produces an equally spaced vector of N increments
+   * in the inclusive range of [a, b]
+   */
   double h = (b - a) / static_cast<double>(N - 1);
   std::vector<double> xs(N);
   std::vector<double>::iterator x;

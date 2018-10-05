e65498cae3bac6d6de3b17b5e9a1af91c2c1ed3b
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-10-05 17:30:25 +0300

    Adjusted Makefiles to include tinyxml2 library.

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,13 +1,14 @@
+#include <stdlib.h>
 #include <string>
 #include <thread>
 #include "MD.h"
+#include "tinyxml2.h"
 
-#define STEPS 10000     //10000
+using namespace tinyxml2;
 
-/* Linux working directory */
-std::string dir = "/home/gn/Desktop/test_data/sample/";
+int main(int argc, char const* argv[]) {
+  /* Path and name of the XML schema file */
 
-int main(int argc, char const *argv[]) {
-  MD run(dir, STEPS);
-  run.Simulation(0.5, 0.5, 6, 0.5);
+  XMLDocument doc;
+  doc.LoadFile("input_schema.xml");
 }

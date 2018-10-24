598a642c92250e8988291d45c49925d8c6d4ac95
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-10-24 21:39:08 +0300

    included again the sampling over various temperatures for multiple density compressions.

--- examples/fluid_compression.cpp
+++ examples/fluid_compression.cpp
@@ -7,28 +7,32 @@
 typedef std::vector<double> vec1d;
 
 /* Linux working directory */
-std::string dir_linux = "./example_data/";
+std::string dir_linux = "/home/gn/Code/MD-simulation/examples/example_data";
 
 std::vector<double> LinearSpacedArray(double a, double b, std::size_t N);
 
 int main() {
-  vec1d temp = LinearSpacedArray(0.001, 0.01, 10);
-  for (size_t i = 0; i < temp.size(); i++) {
+  vec1d temperature_array = LinearSpacedArray(0.001, 0.01, 10);
+  for (const auto& i : temperature_array) {
     MD* run1 = new MD(dir_linux, STEPS, true);  // fluid compression set to true
     MD* run2 = new MD(dir_linux, STEPS, true);
     MD* run3 = new MD(dir_linux, STEPS, true);
     MD* run4 = new MD(dir_linux, STEPS, true);
 
-    std::thread th1(&MD::Simulation, run1, 0.05, 0.5, 12, 0.0);
-    std::thread th2(&MD::Simulation, run2, 0.05, 0.5, 12, 0.5);
-    std::thread th3(&MD::Simulation, run3, 0.05, 0.5, 12, 0.1);
-    std::thread th4(&MD::Simulation, run4, 0.05, 0.5, 12, 1.5);
+    std::thread th1(&MD::Simulation, run1, 0.05, i, 12, 0.25);
+    std::thread th2(&MD::Simulation, run2, 0.05, i, 12, 0.5);
+    std::thread th3(&MD::Simulation, run3, 0.05, i, 12, 0.1);
+    std::thread th4(&MD::Simulation, run4, 0.05, i, 12, 1.25);
 
     th1.join();
     th2.join();
     th3.join();
     th4.join();
-    delete run1, run2, run3, run4;
+
+    delete run1;
+    delete run2;
+    delete run3;
+    delete run4;
   }
 }
 

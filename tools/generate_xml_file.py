f7a5a2b15560fd7716189055c8dace55d094888c
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-11-01 20:12:09 +0000

    Added functionality that uses the full potential of the constructor

--- tools/generate_xml_file.py
+++ tools/generate_xml_file.py
@@ -8,12 +8,17 @@ import xml.etree.ElementTree as ET
 # TEMPERATURE = sys.argv[4]
 # POWER = sys.argv[5]
 # A = sys.argv[6]
-# TODO: add exception handling 
+# TODO: add exception handling
 # TODO: add int __main__ for file execution from the terminal
 
 DIR = "/home/gn/Code/MD-simulation/examples/example_data"
-STEPS = 5000
+STEPS = 15000
 COMPRESSION = "false"
+RDF_BINS = 500
+PARTICLES_PER_AXIS = 10
+TRACK_PARTICLES = "false"
+RDF_EQUILIBRATE = 2000
+
 RHO = 0.5
 TEMPERATURE = 0.5
 POWER = 6
@@ -27,6 +32,10 @@ input = ET.SubElement(root, "constructor")
 ET.SubElement(input, "output_dir").text = str(DIR)
 ET.SubElement(input, "steps").text = str(STEPS)
 ET.SubElement(input, "compression").text = COMPRESSION
+ET.SubElement(input, "rdf_bins").text = str(RDF_BINS)
+ET.SubElement(input, "particles_per_axis").text = str(PARTICLES_PER_AXIS)
+ET.SubElement(input, "track_particles").text = TRACK_PARTICLES
+ET.SubElement(input, "rdf_equilibrate").text = str(RDF_EQUILIBRATE)
 
 # Input parameters for MD::Simulation
 sim = ET.SubElement(root, "simulation_input")

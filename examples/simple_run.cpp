98bfc9964543fb22ce9960ca13814470b4593836
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-11-12 18:26:40 +0000

    Included more examples in the examples directory

---
+++ examples/simple_run.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include "MD.h"
+
+/* 
+ * A demonstration of how the MD class should be used.
+ * It creates 3 files in the directory.
+ * One containing all the state properties of the fluid,
+ * along with the Mean Square Displacement and the
+ * Velocity Autocorrelation Fucntion.
+ * A second file containing the Radial Distribution Function
+ * and a third file containing the positions, velocities
+ * and accelarations of the particles at the last time step.
+ */
+
+int main() {
+  std::string dir = "./example_data";
+  size_t steps = 20000;
+  MD run(dir, steps, false, 500, 10, false, 2000);
+  run.Simulation(0.5, 0.5, 8, 0.5);
+}
---
+++ examples/visualise_fluid.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include "MD.h"
+
+/* 
+ * This example creates the normal data files that can be used
+ * to statistically analyse the fluid. In addition, to those files,
+ * 3 files containing the positions of the particles over time are also
+ * created. The script visualise_fluid.py in the tools directory can be
+ * used to produce a 3D scatter plot annimation of the fluid. 
+ */
+
+int main() {
+  std::string dir = "./example_data";
+  size_t steps = 5000;
+  MD run(dir, steps, false, 100, 10, true, 0);
+  run.Simulation(0.5, 0.5, 8, 0.5);
+}

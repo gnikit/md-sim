014b0bdb2b9b2740cfdd849ff48ecc2f137992dd
giannisnikiteas <giannis_nikiteas@hotmail.com>
2018-11-13 04:08:18 +0000

    Changed the output directory so that it can be executed from the top of the MD-simulation project.  Also fixed spelling mistakes.

--- examples/visualise_fluid.cpp
+++ examples/visualise_fluid.cpp
@@ -6,11 +6,11 @@
  * to statistically analyse the fluid. In addition, to those files,
  * 3 files containing the positions of the particles over time are also
  * created. The script visualise_fluid.py in the tools directory can be
- * used to produce a 3D scatter plot annimation of the fluid. 
+ * used to produce a 3D scatter plot animation of the fluid. 
  */
 
 int main() {
-  std::string dir = "./example_data";
+  std::string dir = "./examples/example_data";
   size_t steps = 5000;
   MD run(dir, steps, false, 100, 10, true, 0);
   run.Simulation(0.5, 0.5, 8, 0.5);

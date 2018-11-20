e5ac3924760362b1f3c425439d523c350543bbd9
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-11-20 07:20:10 +0000

    Changed from GCM to BIP model.

--- examples/fluid_compression.cpp
+++ examples/fluid_compression.cpp
@@ -1,38 +1,25 @@
-#include <thread>
-#include "MD.h"
-// BUG: potential bug, including 2 different math libraries if intel compiler
-#include <math.h>
 #include <iostream>
+#include "MD.h"
+
+#define STEPS_PER_COMPRESSION 5000
 #define PARTICLES 1000
 typedef std::vector<double> vec1d;
 
 /* Linux working directory */
-std::string dir_linux = "/home/gn/Desktop/test_data/gaussian";
+std::string dir_linux = "/home/gn/Desktop/compress";
 
 std::vector<double> LinearSpacedArray(double a, double b, std::size_t N);
 
-// TODO: Insert the individual number of steps per compression and the initial
-// and final compression limits, along with increments. Then let the simulation
-// run
 int main() {
-  vec1d temperature_array = LinearSpacedArray(0.001, 0.01, 10);
-
-  unsigned int COMPRESS_EVERY = 1000;
-  double rho = 0.05;
-  double final_rho = 0.8;
-  double rho_increment = 0.001;
-  unsigned int compression_num = ceil((final_rho - rho) / rho_increment);
-  unsigned int STEPS = compression_num * COMPRESS_EVERY;
-
-  std::cout << "Initial density: " << rho << "\nFinal density: " << final_rho
-            << "\nDensity increment: " << rho_increment
-            << "\nTotal steps: " << STEPS
-            << "\nCompress every: " << COMPRESS_EVERY << std::endl;
-
+  vec1d temperature_array = LinearSpacedArray(0.006, 0.01, 1);
+  /* 
+   * Adjust the DENSITY, FINAL_DENSITY for the fluid->solid transition and then
+   * re-adjust it for the solid->fluid. Otherwise the simulations will sample
+   * a lot of unneeded densities.
+   */
   for (const auto& i : temperature_array) {
-    MD* run1 = new MD(dir_linux, STEPS, true);  // fluid compression set to true
-    run1->steps_per_compress = COMPRESS_EVERY;
-    run1->Simulation(rho, i, 12, 0);
+    MD* run1 = new MD(dir_linux, STEPS_PER_COMPRESSION, true);
+    run1->get_phases(0.05, 0.2, 0.001, i, 12, 0);
   }
 }
 

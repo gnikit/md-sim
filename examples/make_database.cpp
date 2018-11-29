0a0169425f3af1f431512fe3328c0f1648c5e39a
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2018-11-29 13:23:30 +0000

    Configured the database and MD class to run on the exponential pair potential. The cutoff has been decreased, the pair potential has been changed and  the make_database.cpp has been altered in order to produce accurate results.

--- examples/make_database.cpp
+++ examples/make_database.cpp
@@ -2,11 +2,12 @@
 #include <string>
 #include <thread>
 #include "MD.h"
+#include <mathimf.h>
 
-#define STEPS 35000  // 10000
-#define RDF_EQ 5000
+#define STEPS 65000  // 10000
+#define RDF_EQ 15000
 
-std::string dir_linux = "/home/gn/Desktop/test_data";
+std::string dir_linux = "/home/gn/Desktop/test_data/exp";
 
 void MakeDataBase();
 
@@ -23,10 +24,10 @@ void MakeDataBase() {
    */
 
   size_t num = 1;
-  std::vector<size_t> n = {8, 10, 12};
-  std::vector<double> rho = {0.20, 0.3, 0.5, 1.0};
+  std::vector<double> n = {1.5, 2.0, 2.5, 3.0/* 8, 10, 12 */};
+  std::vector<double> rho = {0.20, 0.3, 0.5, 0.8, 1.0};
   /* Test temperature smaller than 1 line 0.75 */
-  std::vector<double> T = {0.5, 1.0, 1.5, 2.0};
+  std::vector<double> T = {1/* 0.5, 1.0, 1.5, 2.0 */};
   std::vector<double> A1 = LinearSpacedArray(0, 1, 5);
   std::vector<double> A2 = LinearSpacedArray(1.25, 2.25, 5);
   std::vector<double> A3 = LinearSpacedArray(2.50, 4.50, 5);
@@ -35,32 +36,32 @@ void MakeDataBase() {
   for (size_t d = 0; d < rho.size(); ++d) {
     for (size_t t = 0; t < T.size(); ++t) {
       for (size_t i = 0; i < n.size(); ++i) {
-        for (size_t j = 0; j < A1.size(); ++j) {
+        // for (size_t j = 0; j < A1.size(); ++j) {
           std::cout << " run num: " << num << std::endl;
 
-          MD* run1 = new MD(dir_linux, STEPS, false, 500, 10, false, RDF_EQ);
-          MD* run2 = new MD(dir_linux, STEPS, false, 500, 10, false, RDF_EQ);
-          MD* run3 = new MD(dir_linux, STEPS, false, 500, 10, false, RDF_EQ);
-          MD* run4 = new MD(dir_linux, STEPS, false, 500, 10, false, RDF_EQ);
+          MD* run1 = new MD(dir_linux, STEPS, false, 250, 8, false, RDF_EQ);
+          MD* run2 = new MD(dir_linux, STEPS, false, 250, 8, false, RDF_EQ);
+          // MD* run3 = new MD(dir_linux, STEPS, false, 500, 10, false, RDF_EQ);
+          // MD* run4 = new MD(dir_linux, STEPS, false, 500, 10, false, RDF_EQ);
 
           // ? set temperature manually in default machine
-          std::thread th1(&MD::Simulation, run1, rho[d], T[t], n[i], A1[j]);
-          std::thread th2(&MD::Simulation, run2, rho[d], T[t], n[i], A2[j]);
-          std::thread th3(&MD::Simulation, run3, rho[d], T[t], n[i], A3[j]);
-          std::thread th4(&MD::Simulation, run4, rho[d], T[t], n[i], A4[j]);
+          std::thread th1(&MD::Simulation, run1, rho[d], T[t], n[i], exp(0.25));
+          std::thread th2(&MD::Simulation, run2, rho[d], T[t], n[i], exp(0.75));
+          // std::thread th3(&MD::Simulation, run3, rho[d], T[t], n[i], A3[j]);
+          // std::thread th4(&MD::Simulation, run4, rho[d], T[t], n[i], A4[j]);
 
           th1.join();
           th2.join();
-          th3.join();
-          th4.join();
+          // th3.join();
+          // th4.join();
 
           delete run1;
           delete run2;
-          delete run3;
-          delete run4;
+          // delete run3;
+          // delete run4;
 
           ++num;
-        }
+        // }
       }
     }
   }

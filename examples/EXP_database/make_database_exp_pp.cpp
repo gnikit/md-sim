716e990c08a94ae3c9b0f384c4eeee7541f243e9
Giannis Nikiteas <giannis_nikiteas@hotmail.com>
2019-05-16 20:03:31 +0100

    Fixed compilation error

--- examples/EXP_database/make_database_exp_pp.cpp
+++ examples/EXP_database/make_database_exp_pp.cpp
@@ -19,7 +19,6 @@ std::string dir_linux = ".";
 
 void MakeDataBase();
 
-
 int main() { MakeDataBase(); }
 
 void MakeDataBase() {
@@ -43,36 +42,25 @@ void MakeDataBase() {
   for (size_t d = 0; d < rho.size(); ++d) {
     for (size_t t = 0; t < T.size(); ++t) {
       for (size_t i = 0; i < n.size(); ++i) {
-        // for (size_t j = 0; j < A1.size(); ++j) {
         std::cout << " run num: " << num << std::endl;
 
         MD* run1 = new MD(dir_linux, STEPS, false, 250, 8, "SC", false, RDF_EQ);
         MD* run2 = new MD(dir_linux, STEPS, false, 250, 8, "SC", false, RDF_EQ);
-        MD* run3 = new MD(dir_linux, STEPS, false, 500, 8, "SC", false, RDF_EQ);
-        MD* run4 = new MD(dir_linux, STEPS, false, 500, 8, "SC", false, RDF_EQ);
 
         // ? set temperature manually in default machine
         std::thread th1(&MD::Simulation, run1, rho[d], T[t], n[i], exp(0.25),
                         "EXP");
         std::thread th2(&MD::Simulation, run2, rho[d], T[t], n[i], exp(0.75),
                         "EXP");
-        std::thread th3(&MD::Simulation, run3, rho[d], T[t], n[i], A3[j]);
-        std::thread th4(&MD::Simulation, run4, rho[d], T[t], n[i], A4[j]);
 
         th1.join();
         th2.join();
-        th3.join();
-        th4.join();
 
         delete run1;
         delete run2;
-        delete run3;
-        delete run4;
 
         ++num;
-        // }
       }
     }
   }
 }
-

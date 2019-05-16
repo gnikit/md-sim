9367db5ec28526271ec381e483328ceee6577a03
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2019-05-16 20:00:16 +0100

    Replaced LinearSpacedArray calls with linspace

--- examples/EXP_database/make_database_exp_pp.cpp
+++ examples/EXP_database/make_database_exp_pp.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <thread>
 #include "MD.h"
+#include "helper_functions.h"
 
 // Load Intel math lib if available
 #if defined(__INTEL_COMPILER)
@@ -18,7 +19,6 @@ std::string dir_linux = ".";
 
 void MakeDataBase();
 
-std::vector<double> LinearSpacedArray(double a, double b, std::size_t N);
 
 int main() { MakeDataBase(); }
 
@@ -35,9 +35,9 @@ void MakeDataBase() {
   std::vector<double> rho = {0.20, 0.3, 0.5, 0.8, 1.0};
   /* Test temperature smaller than 1 line 0.75 */
   std::vector<double> T = {1 /* 0.5, 1.0, 1.5, 2.0 */};
-  std::vector<double> A1 = LinearSpacedArray(0, 1, 5);
-  std::vector<double> A2 = LinearSpacedArray(1.25, 2.25, 5);
-  std::vector<double> A3 = LinearSpacedArray(2.50, 4.50, 5);
+  std::vector<double> A1 = helper_functions::linspace(0.0, 1.0, 5);
+  std::vector<double> A2 = helper_functions::linspace(1.25, 2.25, 5);
+  std::vector<double> A3 = helper_functions::linspace(2.50, 4.50, 5);
   std::vector<double> A4 = {0.20, 0.40, 0.60, 0.80, 0.90};
 
   for (size_t d = 0; d < rho.size(); ++d) {
@@ -48,26 +48,26 @@ void MakeDataBase() {
 
         MD* run1 = new MD(dir_linux, STEPS, false, 250, 8, "SC", false, RDF_EQ);
         MD* run2 = new MD(dir_linux, STEPS, false, 250, 8, "SC", false, RDF_EQ);
-        // MD* run3 = new MD(dir_linux, STEPS, false, 500, 10, false, RDF_EQ);
-        // MD* run4 = new MD(dir_linux, STEPS, false, 500, 10, false, RDF_EQ);
+        MD* run3 = new MD(dir_linux, STEPS, false, 500, 8, "SC", false, RDF_EQ);
+        MD* run4 = new MD(dir_linux, STEPS, false, 500, 8, "SC", false, RDF_EQ);
 
         // ? set temperature manually in default machine
         std::thread th1(&MD::Simulation, run1, rho[d], T[t], n[i], exp(0.25),
                         "EXP");
         std::thread th2(&MD::Simulation, run2, rho[d], T[t], n[i], exp(0.75),
                         "EXP");
-        // std::thread th3(&MD::Simulation, run3, rho[d], T[t], n[i], A3[j]);
-        // std::thread th4(&MD::Simulation, run4, rho[d], T[t], n[i], A4[j]);
+        std::thread th3(&MD::Simulation, run3, rho[d], T[t], n[i], A3[j]);
+        std::thread th4(&MD::Simulation, run4, rho[d], T[t], n[i], A4[j]);
 
         th1.join();
         th2.join();
-        // th3.join();
-        // th4.join();
+        th3.join();
+        th4.join();
 
         delete run1;
         delete run2;
-        // delete run3;
-        // delete run4;
+        delete run3;
+        delete run4;
 
         ++num;
         // }
@@ -76,17 +76,3 @@ void MakeDataBase() {
   }
 }
 
-std::vector<double> LinearSpacedArray(double a, double b, std::size_t N) {
-  /*
-   * Produces an equally spaced vector of N increments
-   * in the inclusive range of [a, b]
-   */
-  double h = (b - a) / static_cast<double>(N - 1);
-  std::vector<double> xs(N);
-  std::vector<double>::iterator x;
-  double val;
-  for (x = xs.begin(), val = a; x != xs.end(); ++x, val += h) {
-    *x = val;
-  }
-  return xs;
-}

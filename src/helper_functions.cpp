809a817e17aee1e2f7cf42ed038f371feaaf30e7
Giannis Nikiteas <giannis_nikiteas@hotmail.com>
2019-07-29 00:24:48 +0100

    Added a linear fit method

--- src/helper_functions.cpp
+++ src/helper_functions.cpp
@@ -29,3 +29,28 @@ std::vector<double> helper_functions::linspace(double a, double b, size_t N) {
   }
   return xs;
 }
+
+std::tuple<double, double> helper_functions::linfit(
+    const std::vector<double>& x, const std::vector<double>& y) {
+  /*
+   * Calculates a simple linear regression fit
+   * and returns the slope (a) and y-intercept (b) of the fit.
+   */
+
+  // Test the supplied vectors are of equal length
+  if (x.size() != y.size()) throw "X and Y are unequal sizes";
+
+  const auto n = x.size();
+  const auto s_x = std::accumulate(x.begin(), x.end(), 0.0);
+  const auto s_y = std::accumulate(y.begin(), y.end(), 0.0);
+  const auto s_xx = std::inner_product(x.begin(), x.end(), x.begin(), 0.0);
+  const auto s_xy = std::inner_product(x.begin(), x.end(), y.begin(), 0.0);
+
+  const auto denominator = (n * s_xx - s_x * s_x);
+  if (denominator == 0) throw "Denominator is 0";
+
+  // Slope of the linear fit
+  const auto a = (n * s_xy - s_x * s_y) / denominator;
+  // Y-intercept of the linear fit
+  const auto b = (s_y * s_xx - s_x * s_xy) / denominator;
+}

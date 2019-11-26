7a10920c5fca058ed5655e0bc5a7ba726f56323a
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2019-11-26 17:32:33 +0000

    Fixed printing bug.

--- src/phase_transition.cpp
+++ src/phase_transition.cpp
@@ -46,7 +46,7 @@ void phase_transition::crystallisation(std::string SIMULATION_NAME,
 
   for (size_t comp_step = 1; comp_step < total_comp_steps; comp_step++) {
     std::cout << "Runing MD::simulation " << comp_step << "/"
-              << total_comp_steps << std::endl;
+              << total_comp_steps -1 << std::endl;
 
     simulation(SIMULATION_NAME, current_rho, TEMPERATURE, POWER, A_CST,
                pp_type);

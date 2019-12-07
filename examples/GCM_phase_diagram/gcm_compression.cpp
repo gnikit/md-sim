6015088234c31bf22ee98ca41bff0595c034233a
Giannis Nikiteas <giannis.nikiteas@gmail.com>
2019-12-07 20:27:48 +0000

    Changes the compression steps a bit.

--- examples/GCM_phase_diagram/gcm_compression.cpp
+++ examples/GCM_phase_diagram/gcm_compression.cpp
@@ -12,12 +12,17 @@ int main() {
    * density iterations by equating the compression timestep to 1
    *
    */
-  phase_transition run(compress_steps, 5, "FCC");
+  phase_transition run(compress_steps, 7, "FCC");
   run.set_compression_flag(true);
 
-  std::vector<double> temperatures = {0.0033, 0.0035};
+  std::vector<double> temperatures = {
+      4.602932914279152937e-05, 6.461042397802770065e-05,
+      6.454528429411781482e-05, 7.688925439500345071e-05,
+      8.923322449588821925e-05, 1.078306042521020788e-04};
+
   for (size_t t = 0; t < temperatures.size(); ++t) {
-    run.crystallisation("compress_fwd_", 0.05, 0.20, 0.025, temperatures[t],
-                        NAN, NAN, "GCM");
+    run.crystallisation("compress_fwd_" + std::to_string(t), 3.40e-02, 4.2e-02,
+                        double(abs(3.40e-02 - 4.2e-02) / 8),
+                        temperatures[t], NAN, NAN, "GCM");
   }
 }
